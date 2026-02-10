fca65c8973ab23565ecdd3154b9229988ff22b9f
waaajih <hijawnassah@gmail.com>
2026-02-10 16:12:42 +0000

    Restored

--- main/development/build1/src/main/java/Group4/tracer/controller/TracerController.java
+++ main/development/build1/src/main/java/Group4/tracer/controller/TracerController.java
@@ -26,8 +26,12 @@ public class TracerController {
             Object[] productData = productRepository.findProductArray(userInput); //stores array data
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             //Object[] traceData = productRepository.
             //I had to use ToString() to output the array or else it would print an memory address instead
+=======
+
+>>>>>>> Stashed changes
 =======
 
 >>>>>>> Stashed changes
