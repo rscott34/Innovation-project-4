efdbe4000d1128a97289d5bec31cc4189972df24
waaajih <hijawnassah@gmail.com>
2026-02-11 18:06:35 +0000

    Added imports

--- main/development/build1/src/main/java/Group4/tracer/controller/TracerController.java
+++ main/development/build1/src/main/java/Group4/tracer/controller/TracerController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import Group4.tracer.repository.ProductRepository;
 import Group4.tracer.repository.StageRepository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class TracerController {
 
